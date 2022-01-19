304126615cff7623576c95f2b081f9ec77632f0e
lintzuyun <tzuyunlin.tw@gmail.com>
2022-01-19 12:01:44 +0000

    exercise level 1-2 completed

---
+++ Level1/2.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	// exercise #2 -1
+
+	var x int
+	var y string
+	var z bool
+
+	// exercise #2 -2
+
+	fmt.Println(x)
+	fmt.Println(y)
+	fmt.Println(z)
+
+	/*
+		Q - The compiler assigned values to the variables. ^ What are these values called?
+		A - zero value.
+	*/
+}
