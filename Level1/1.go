f95d87371877d47e406988c6b994870546cfecf5
lintzuyun <tzuyunlin.tw@gmail.com>
2022-01-19 11:50:04 +0000

    exercise level 1-1 completed

---
+++ Level1/1.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	// exercise #1 -1
+
+	x := 42
+	y := "James Bond"
+	z := true
+
+	// exercise #1 -2
+	fmt.Println(x)
+	fmt.Println(y)
+	fmt.Println(z)
+
+	fmt.Println(x, y, z)
+}
