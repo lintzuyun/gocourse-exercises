a40b797ba8769b3439ea86440feea89288d67a06
lintzuyun <tzuyunlin.tw@gmail.com>
2022-01-20 18:26:29 +0000

    exercise level 2-3 completed

---
+++ Level2/3.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	// exercise #3
+	const (
+		x int = 12 // create typed constant
+		y     = 13 // create untyped constant
+	)
+
+	fmt.Println(x, y)
+}
