2ca6aa874e130187816839e59b37c975a85d721d
lintzuyun <tzuyunlin.tw@gmail.com>
2022-01-23 20:28:03 +0000

    exercise level 3-6 completed

---
+++ Level3/6.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	// exercise #6
+	// create a program that shows the "if statement" in action
+
+	x := "Joy"
+	if x == "Joy" {
+		fmt.Println(x)
+	}
+}
