cff5a7a3f4813972b0e21a365b21b20601d29e02
lintzuyun <tzuyunlin.tw@gmail.com>
2022-01-23 20:35:43 +0000

    exercise level 3-7 completed

---
+++ Level3/7.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	// exercise #7
+	// build on the previous one, use "else if" and "else"
+
+	x := "Joy"
+	if x == "a" {
+		fmt.Println("This is a")
+	} else if x == "b" {
+		fmt.Println("This is b")
+	} else {
+		fmt.Println("Joooooooy", x)
+	}
+}
