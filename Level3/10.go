16453f9bb8006add7998f7eafacdaf770293bffc
lintzuyun <tzuyunlin.tw@gmail.com>
2022-01-24 09:22:23 +0000

    exercise level 3-10 completed

---
+++ Level3/10.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	// exercise #10
+	fmt.Println(true && true)
+	fmt.Println(true && false)
+	fmt.Println(true || true)
+	fmt.Println(true || false)
+	fmt.Println(!true)
+}
+
+// write down what these print
+/*
+true
+false
+true
+true
+false
+*/
